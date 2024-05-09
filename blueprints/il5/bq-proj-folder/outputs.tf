4fddf575d2084182925569940173b7511e3b166e
Ashley Siddiqui <ashley.siddiqui@darkwolfsolutions.com>
2024-05-09 02:20:20 -0400

    Cleaned up files

---
+++ blueprints/il5/bq-proj-folder/outputs.tf
@@ -0,0 +1,24 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+output "dataset_id" {
+  value = dataset_id.dataset_01
+}
+output "google_kms_key_ring" {
+  value = google_kms_key_ring.may-big-query-key-ring
+}
+output "google_kms_crypto_key" {
+  value = google_kms_crypto_key.crypto_bq_key
+}
