8fd911c4e906759fa699d26e400a09ecdb11ef4e
Dan Gordon <dan.gordon@darkwolfsolutions.com>
2024-05-20 15:32:53 -0400

    added depends on

--- blueprints/il5/bq-proj-folder/main.tf
+++ blueprints/il5/bq-proj-folder/main.tf
@@ -13,43 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #Terraform Provider for Google Cloud Platform
 provider "google" {
   project = var.project_id
   region  = var.region
 } #data #resources #module #commented out code 
+
 #Google KMS Module
 module "kms" {
   source     = "../../../modules/kms"
   project_id = var.project_id
   keys       = var.keys 
   iam = {
-    "roles/cloudkms.cryptoKeyEncrypterDecrypter" = ["user:${data.google_bigquery_default_service_account.bq_sa.member}","serviceAccount:${data.google_bigquery_default_service_account.bq_sa.email}"]
+    "roles/cloudkms.cryptoKeyEncrypterDecrypter" = ["serviceAccount:${data.google_bigquery_default_service_account.bq_sa.email}"]
   }
   keyring    = var.keyring
   }
 data "google_project" "current" {}
 data "google_bigquery_default_service_account" "bq_sa" {}
 
-# resource "google_kms_crypto_key_iam_member" "key_sa_user" {
-#   crypto_key_id = module.kms.keys.default.id
-#   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-#   member        = "serviceAccount:${data.google_bigquery_default_service_account.bq_sa.email}"
-# }
-# Obtain the Cloud Storage Service Account
-# locals {
-#   big_query_service_account = "service-${data.google_project.current.number}@bigquery-encryption.iam.gserviceaccount.com"
-# }
-
-# Google IAM Policy to the user as Admin
-# data "google_iam_policy" "admin" {
-#   binding {
-#     role = "roles/storage.admin"
-#     members = [
-#       "user:${var.email}"
-#     ]
-#   }
-# }
 module "bigquery-dataset" {
   source                     = "../../../modules/bigquery-dataset"
   location                   = "us-east4"
@@ -57,13 +40,6 @@ module "bigquery-dataset" {
   id                         = var.id
   description                = "This dataset has customer managed encrytped keys, is updated in real-time, and accessed by restricted roles."
   encryption_key             = module.kms.keys.default.id
+
+  depends_on = [module.kms]
 }
-
-
-
-
-
-
-
-
-
