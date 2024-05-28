bfaad563e3329eb59c47340e4c233d6c232db28b
Will Kline <will.kline@darkwolfsolutions.com>
2024-05-28 14:05:19 -0400

    oops, forgot to actually add the bastion.tf file

---
+++ fast/stages-aw/2-networking-f-ngfw/bastion.tf
@@ -0,0 +1,29 @@
+# Google Compute Shielded VM Module
+module "bastion-vm" {
+  source     = "../../../modules/compute-vm"
+  project_id = module.landing-project.project_id
+  zone       = "${var.regions["primary"]}-a"
+  name       = "management-bastion"
+  shielded_config = {
+    enable_secure_boot          = true
+    enable_vtpm                 = true
+    enable_integrity_monitoring = true
+  }
+  instance_type = "e2-medium"
+  network_interfaces = [{
+    network = module.mgmt-vpc.self_link
+    subnetwork = try(
+      module.mgmt-vpc.subnet_self_links["${var.regions["primary"]}/mgmt-default"], null
+    )
+  }]
+  attached_disks = [
+    {
+      auto_delete = true
+      size        = 10
+      name        = "data-disk"
+      initialize_params = {
+        image = "projects/cos-cloud/global/images/family/cos-stable"
+      }
+    }
+  ]
+}
