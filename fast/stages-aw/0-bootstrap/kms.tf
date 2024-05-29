6d3a8a9f566b61e6637f03808c69fb47ea5766de
David White <whitedavid@google.com>
2024-05-29 20:13:16 +0000

    chore: fmt

--- fast/stages-aw/0-bootstrap/kms.tf
+++ fast/stages-aw/0-bootstrap/kms.tf
@@ -1,6 +1,6 @@
 locals {
   version_template = {
-   algorithm = "GOOGLE_SYMMETRIC_ENCRYPTION"
+    algorithm        = "GOOGLE_SYMMETRIC_ENCRYPTION"
     protection_level = "HSM"
   }
 }
@@ -20,15 +20,15 @@ module "kms" {
   iam_bindings_additive = {
     "pubsub" = {
       member = "serviceAccount:service-${module.log-export-project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
-      role = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+      role   = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
     },
     "bq" = {
       member = "serviceAccount:bq-${module.log-export-project.number}@bigquery-encryption.iam.gserviceaccount.com"
-      role = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+      role   = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
     },
     "gcs" = {
       member = "serviceAccount:service-${module.log-export-project.number}@gs-project-accounts.iam.gserviceaccount.com"
-      role = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+      role   = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
     }
   }
 }
