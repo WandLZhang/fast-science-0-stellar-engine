fc1d2387d275ddf991ac384429c4d4de3faf2ed6
Ashley Siddiqui <ashley.siddiqui@darkwolfsolutions.com>
2024-06-05 15:51:24 -0400

    changed main.tf roles

--- blueprints/il5/pub-sub-project/outputs.tf
+++ blueprints/il5/pub-sub-project/outputs.tf
@@ -1,7 +1,19 @@
-output "crypto_key_id"{
-    value = google_kms_crypto_key.id
-}
-
-output "key_ring_id"{
-    value = google_kms_key_ring.id
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#Terraform Provider for Google Cloud Platform
+output "key_ring_id" {
+  value = google_kms_key_ring.key_ring.id
 }
