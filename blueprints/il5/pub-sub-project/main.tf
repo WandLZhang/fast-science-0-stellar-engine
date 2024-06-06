97a78aa03ed448fde0eaad5ec665f5f07c675eea
Ashley Siddiqui <ashley.siddiqui@darkwolfsolutions.com>
2024-06-06 12:28:18 -0400

    Publisher and subscriber acccounts created, outputs file modified, HSM utilized

--- blueprints/il5/pub-sub-project/main.tf
+++ blueprints/il5/pub-sub-project/main.tf
@@ -19,9 +19,30 @@ provider "google" {
   region  = var.location
 }
 
-resource "google_service_account" "pubsub_sa" {
-  account_id = var.pubsub_service_account_id
-  project    = var.project_id
+#Create publisher account 
+resource "google_service_account" "publisher" {
+  account_id = var.publisher_account_id
+  name       = var.publisher_name
+}
+
+#Create subscriber account 
+resource "google_service_account" "subscriber" {
+  account_id   = var.subscriber_account_id
+  display_name = var.subscriber_name
+}
+
+#IAM role for the publisher service account
+resource "google_project_iam_member" "pubsub_publisher" {
+  project = var.project_id
+  role    = "roles/pubsub.publisher"
+  member  = "serviceAccount:${google_service_account.publisher.email}"
+}
+
+#IAM role for the subscriber service account
+resource "google_project_iam_member" "pubsub_subscriber" {
+  project = var.project_id
+  role    = "roles/pubsub.subscriber"
+  member  = "serviceAccount:${google_service_account.subscriber.email}"
 }
 
 #Google KMS Module 
@@ -31,38 +52,40 @@ module "kms" {
   keys       = var.keys
   iam = {
     "roles/cloudkms.cryptoKeyEncrypterDecrypter" = [
-     google_service_account.pubsub_sa.email
+      google_service_account.publisher.email,
+      google_service_account.subscriber.email
     ]
   }
   keyring = var.keyring
 }
 
-#Implementing CMEK to pub-sub
+# Create a Pub/Sub topic with allowed persistence regions
 resource "google_pubsub_topic" "pubsub_topic" {
   name         = var.pubsub_topic_name
   kms_key_name = module.kms.crypto_key_id
 }
 
+# IAM role bindings for publisher account
+resource "google_pubsub_topic_iam_member" "publisher" {
+  topic  = google_pubsub_topic.pubsub_topic.name
+  role   = "roles/pubsub.publisher"
+  member = "serviceAccount:${google_service_account.publisher.email}"
+}
+
+#IAM role bindings for subscriber account
+resource "google_pubsub_subscription_iam_member" "subscriber" {
+  subscription = google_pubsub_subscription.subscription.name
+  role         = "roles/pubsub.subscriber"
+  member       = "serviceAccount:${google_service_account.subscriber.email}"
+}
+
 # Creating a Pub/Sub subscription
 resource "google_pubsub_subscription" "subscription" {
-  name  = var.pubsub_subscription_name
-  topic = google_pubsub_topic.pubsub_topic.name
+  name                 = var.pubsub_subscription_name
+  topic                = google_pubsub_topic.pubsub_topic.name
   ack_deadline_seconds = 20
   push_config {
     #create push endpoint
     push_endpoint = var.push_endpoint
   }
-}
-
-# IAM role bindings for the service account
-resource "google_pubsub_topic_iam_member" "publisher" {
-  topic      = google_pubsub_topic.pubsub_topic.name
-  role       = "roles/pubsub.publisher"
-  member     = "serviceAccount:${google_service_account.pubsub_sa.email}"
-}
-
-resource "google_pubsub_subscription_iam_member" "subscriber" {
-  subscription = google_pubsub_subscription.subscription.name
-  role         = "roles/pubsub.subscriber"
-  member       = "serviceAccount:${google_service_account.pubsub_sa.email}"
 }
