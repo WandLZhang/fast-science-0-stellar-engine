d512ed4a9f495ee39ce178945a1aafde2bbccfdb
Your Name <tapan.banker@darkwolfsolutions.com>
2024-06-13 13:21:00 -0400

    revision in main for wait condition

--- blueprints/il5/gke-project/main.tf
+++ blueprints/il5/gke-project/main.tf
@@ -3,7 +3,7 @@ provider "google" {
   region  = var.region
 }
 
-# # Create KMS Key Ring and Crypto Key using the kms module
+# Create KMS Key Ring and Crypto Key using the kms module
 module "kms" {
   source     = "../../../modules/kms"
   project_id = var.project_id
@@ -22,7 +22,7 @@ module "vpc" {
   source                  = "../../../modules/net-vpc"
   project_id              = var.project_id
   name                    = "vpc-gke-network"
-  auto_create_subnetworks = true
+  auto_create_subnetworks = false
   subnets = [
     {
       ip_cidr_range = "10.0.0.0/24"
@@ -36,6 +36,15 @@ module "vpc" {
   ]
 }
 
+# Wait for the VPC and subnets to be created before creating the cluster
+resource "null_resource" "wait_for_vpc" {
+  depends_on = [module.vpc]
+  provisioner "local-exec" {
+    command = "echo VPC and subnets are created."
+  }
+}
+
+# Google GKE Cluster Module
 module "cluster-1" {
   source     = "../../../modules/gke-cluster-standard"
   project_id = var.project_id
@@ -43,7 +52,7 @@ module "cluster-1" {
   location   = var.region
   vpc_config = {
     network    = module.vpc.self_link
-    subnetwork = module.vpc.subnets["us-east4/subnet-1"].self_link
+    subnetwork = module.vpc.subnets["${var.region}/subnet-1"].self_link
     secondary_range_names = {
       pods     = "pods"
       services = "services"
@@ -54,8 +63,7 @@ module "cluster-1" {
     master_ipv4_cidr_block = "192.168.0.0/28"
   }
   node_config = {
-    #boot_disk_kms_key = "gke-keyringv1"
-    boot_disk_kms_key = "projects/tnbsea-dev-tapand-dev/locations/us-east4/keyRings/gke-keyringv1/cryptoKeys/gke-keyname"
+    boot_disk_kms_key = "projects/${var.project_id}/locations/${var.region}/keyRings/${var.keyring.name}/cryptoKeys/gke-keyname"
   }
   max_pods_per_node = 32
   private_cluster_config = {
@@ -67,10 +75,11 @@ module "cluster-1" {
   }
 }
 
+# Google GKE Node Pool Module
 module "nodepool" {
   source       = "../../../modules/gke-nodepool"
   project_id   = var.project_id
-  cluster_name = "cluster-1"
+  cluster_name = module.cluster-1.name
   location     = var.region
   name         = "nodepool-1"
 }
