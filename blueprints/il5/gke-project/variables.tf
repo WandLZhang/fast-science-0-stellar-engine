f3d72737566e2368b2fa07902738e0afd681d63a
Your Name <tapan.banker@darkwolfsolutions.com>
2024-06-18 14:01:31 -0400

    read me inst

--- blueprints/il5/gke-project/variables.tf
+++ blueprints/il5/gke-project/variables.tf
@@ -1,73 +1,73 @@
 variable "project_id" {
   description = "The ID of the project in which to create the GKE cluster."
   type        = string
-  default     = "tnbsea-dev-tapand-dev"
+  # default     = "project-id"
 }
 
 variable "email" {
   description = "Email address of the user."
   type        = string
-  default     = "admin.tapan@dino-runner.darkwolfsolutions.com"
+  # default     = "email address @ domain . com"
 }
 
 variable "group_email" {
   description = "Email address of the group."
   type        = string
-  default     = "gcp-devops@dino-runner.darkwolfsolutions.com"
+  # default     = "group email address @ domain . com"
 }
 
 variable "region" {
   description = "The GCP region to use for the resources."
   type        = string
-  default     = "us-east4"
+  # default     = "us-east4"
 }
 
 variable "gke_service_account_id" {
   description = "The GKE Service account"
   type        = string
-  default     = "gke-compute-sa"
+  # default     = "gke-service_account_1"
 }
 
 variable "vpc_name_1" {
   description = "The VPC Name"
   type        = string
-  default     = "vpc-gke-j18"
+  # default     = "vpc-gke-kubernetes"
 }
 
 variable "subnet_name_1" {
   description = "The Subnet Name"
   type        = string
-  default     = "subnet-gke-j18-a"
+  # default     = "subnet-gke-kubernetes"
 }
 
 variable "subnet_ip_cidr_range_1" {
   description = "The CIDR Range for the VPC Subnet"
   type        = string
-  default     = "10.0.4.0/22"
+  # default     = "10.0.4.0/22"
 }
 
 variable "subnet_secondary_ip_range_pods_1" {
   description = "The CIDR Range for the secondary IP CIDR Ranges for the k8s pods"
   type        = string
-  default     = "10.4.0.0/14"
+  # default     = "10.4.0.0/14"
 }
 
 variable "subnet_secondary_ip_range_services_1" {
   description = "The CIDR Range for the secondary IP CIDR Ranges for the k8s services"
   type        = string
-  default     = "10.0.32.0/20"
+  # default     = "10.0.32.0/20"
 }
 
 variable "gke_vpc_master_ipv4_cidr_block" {
   description = "The CIDR Range for the GKE Master IP CIDR Ranges for the k8s used for VPC configuration"
   type        = string
-  default     = "192.168.0.0/28"
+  # default     = "192.168.0.0/28"
 }
 
 variable "gke_cluster_name" {
   description = "The GKE Kubernetes Cluster Name"
   type        = string
-  default     = "gke-k8-j18"
+  # default     = "gke-kubernetes-cls"
 }
 
 variable "gke_initial_node_count" {
@@ -79,20 +79,20 @@ variable "gke_initial_node_count" {
 variable "node_config_tags" {
   description = "The Tags on the Node Configuration"
   type        = list(string)
-  default     = ["node-config-gke", "intial-nodeconfig-gke"]
   nullable    = false
+  # default     = ["node-config-gke", "intial-nodeconfig-gke"]  
 }
 
 variable "gke_nat_name" {
   description = "The GKE Cloud NAT to name"
   type        = string
-  default     = "nat-gke-j18"
+  # default     = "nat-gke-kubernetes"
 }
 
 variable "gke_nodepool_name" {
   description = "The GKE Kubernetes Cluster Name"
   type        = string
-  default     = "gke-nodepoolj18"
+  # default     = "gke-nodepool-k8s"
 }
 
 variable "node_machine_type" {
@@ -104,7 +104,7 @@ variable "node_machine_type" {
 variable "node_disk_size_gb" {
   description = "The disk size in GB to be given to each node"
   type        = number
-  default     = 15
+  # default     = 15
 }
 
 variable "nodepool_node_count" {
@@ -113,10 +113,12 @@ variable "nodepool_node_count" {
     current = optional(number)
     initial = number
   })
-  default = {
-    initial = 1
-  }
   nullable = false
+  # Example  
+  # default = {
+  #   initial = 1
+  # }
+
 }
 
 variable "keyring" {
@@ -125,10 +127,13 @@ variable "keyring" {
     location = string
     name     = string
   })
-  default = {
-    location = "us-east4"
-    name     = "gkekeyring"
-  }
+  # Example  
+  # default = {
+  #  location = "us-east4"
+  #  name     = "update-name-of-keyring"
+  #}
+  # The name of the Key Ring, and location. The Location for IL5 can be us-east4 or us-central1  
+
 }
 
 variable "keys" {
@@ -175,7 +180,7 @@ variable "keys" {
         protection_level = "SOFTWARE"
       }
       iam = {
-        "roles/cloudkms.cryptoKeyEncrypterDecrypter" = ["user:admin.tapan@dino-runner.darkwolfsolutions.com", "group:gcp-devops@dino-runner.darkwolfsolutions.com"]
+        "roles/cloudkms.cryptoKeyEncrypterDecrypter" = ["user:update-your-email-address-here", "group:update-group-email-address-here"]
       }
       lifecycle = {
         prevent_destroy = true
