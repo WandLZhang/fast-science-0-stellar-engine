b31a959a61d977a99885c3438bebb3372ac7799b
Your Name <tapan.banker@darkwolfsolutions.com>
2024-06-20 15:07:06 -0400

    More Read ME instruction

--- blueprints/il5/gke-project/main.tf
+++ blueprints/il5/gke-project/main.tf
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 # Terraform Provider for Google Cloud Platform
 provider "google" {
   project = var.project_id
