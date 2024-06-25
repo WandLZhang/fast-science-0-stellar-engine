d0bac450a8e81f86a5d490783314907cf889de94
Your Name <tapan.banker@darkwolfsolutions.com>
2024-06-25 09:51:11 -0400

    approach on security onion

--- blueprints/il5/gcp-ce-security-onion/main.tf
+++ blueprints/il5/gcp-ce-security-onion/main.tf
@@ -28,45 +28,58 @@ resource "google_service_account" "compute" {
   account_id = var.compute_service_account_id
   project    = var.project_id
 }
+# Google Compute Engine VM Module
+# module "security-onion-vm" {
+#   source        = "../../../modules/compute-vm"
+#   project_id    = var.project_id
+#   zone          = var.zone
+#   name          = "security-on-image-gcp"
+#   instance_type = var.instance_type
 
-#Bastion compute instance 
-module "bastion-vm" {
-  source     = "../../../modules/compute-vm"
-  project_id = var.project_id
-  zone       = var.zone
-  name       = "bastion-test"
+#   boot_disk = {
+#     initialize_params = {
+#       size = 200
+#       image = "security-onion-2" # Use an appropriate Ubuntu image
+#       type  = "pd-ssd"
+#     }
+#   }
 
-  instance_type = var.instance_type
-  network_interfaces = [{
-    # network    = data.google_compute_network.my_vpc.self_link
-    # subnetwork = data.google_compute_subnetwork.my_subnet.self_link
-    network    = module.vpc.network.self_link
-    subnetwork = "projects/${var.project_id}/regions/${var.location}/subnetworks/subnet-onion-a"
-  }]
+#   network_interfaces = [{
+#     network    = module.vpc.network.self_link
+#     subnetwork = "projects/${var.project_id}/regions/${var.location}/subnetworks/subnet-onion-a"
+#   }]
+#   # Define metadata, including the startup script
+#   metadata = {
+#     startup-script = file("${path.module}/startup-script.sh")
+#   }
+#   tags = ["security-onion"]
 
-  service_account = {
-    email = google_service_account.compute.email
-  }
+#   encryption = {
+#     #kms_key_self_link = module.kms.keys.key-so.id
+#     kms_key_self_link = "projects/${var.project_id}/locations/${var.location}/keyRings/${var.keyring.name}/cryptoKeys/key-so"
+#   }
+#   service_account = {
+#     email = google_service_account.compute.email
+#   }
+#   # # Persistent Disk Attached to the Compute Engine with KMS
+#   # attached_disks = [
+#   #   {
+#   #     auto_delete = var.auto_delete
+#   #     size        = 100
+#   #     name        = "data-disk"
+#   #     initialize_params = {       
+#   #       image = "ubuntu-2004-focal-v20240614"       
+#   #     }
+#   #     #kms_key_self_link = module.kms.keys.key-so.id
+#   #     kms_key_self_link = "projects/${var.project_id}/locations/${var.location}/keyRings/${var.keyring.name}/cryptoKeys/key-so"
+#   #   }
+#   # ]
 
-  #Lockdown configuration
-  encryption = {
-    kms_key_self_link = "projects/${var.project_id}/locations/${var.location}/keyRings/${var.keyring.name}/cryptoKeys/key-so"
-  }
-  attached_disks = [
-    {
-      auto_delete = true
-      size        = 100
-      name        = "bastion-disk"
-      initialize_params = {
-        image = "ubuntu-2004-focal-v20240614"
-      }
-      kms_key_self_link = "projects/${var.project_id}/locations/${var.location}/keyRings/${var.keyring.name}/cryptoKeys/key-so"
-    }
-  ]
-}
+#   #depends_on = [module.kms, google_service_account.compute]
+#   depends_on = [google_service_account.compute, module.vpc, module.nat]
+# }
 
 # Google Compute Engine VM Module
-
 module "compute-engine-vm" {
   source        = "../../../modules/compute-vm"
   project_id    = var.project_id
@@ -76,9 +89,7 @@ module "compute-engine-vm" {
 
   boot_disk = {
     initialize_params = {
-      #image = "debian-cloud/debian-10"
-      size = 200
-      #image = "https://www.googleapis.com/compute/v1/projects/centos-cloud/global/images/centos-7-v20240611"
+      size  = 200
       image = "ubuntu-2004-focal-v20240614" # Use an appropriate Ubuntu image
       type  = "pd-ssd"
     }
@@ -87,11 +98,6 @@ module "compute-engine-vm" {
   network_interfaces = [{
     network    = module.vpc.network.self_link
     subnetwork = "projects/${var.project_id}/regions/${var.location}/subnetworks/subnet-onion-a"
-    access_config = {
-      # Enabling external IP
-      nat_ip = "10.200.12.128"
-      #nat_ip =  google_compute_address.public_ip.address    
-    }
   }]
   # Define metadata, including the startup script
   metadata = {
@@ -113,9 +119,59 @@ module "compute-engine-vm" {
       size        = 100
       name        = "data-disk"
       initialize_params = {
-        #image = "debian-cloud/debian-10"
         image = "ubuntu-2004-focal-v20240614"
-        #image = "https://www.googleapis.com/compute/v1/projects/centos-cloud/global/images/centos-7-v20240611"
+      }
+      #kms_key_self_link = module.kms.keys.key-so.id
+      kms_key_self_link = "projects/${var.project_id}/locations/${var.location}/keyRings/${var.keyring.name}/cryptoKeys/key-so"
+    }
+  ]
+
+  #depends_on = [module.kms, google_service_account.compute]
+  depends_on = [google_service_account.compute, module.vpc, module.nat]
+}
+
+
+# Google Compute Engine VM Module
+module "compute-engine-vm-one" {
+  source        = "../../../modules/compute-vm"
+  project_id    = var.project_id
+  zone          = var.zone
+  name          = "second-ubuntu-script-one"
+  instance_type = var.instance_type
+
+  boot_disk = {
+    initialize_params = {
+      size  = 200
+      image = "ubuntu-2004-focal-v20240614" # Use an appropriate Ubuntu image
+      type  = "pd-ssd"
+    }
+  }
+
+  network_interfaces = [{
+    network    = module.vpc.network.self_link
+    subnetwork = "projects/${var.project_id}/regions/${var.location}/subnetworks/subnet-onion-a"
+  }]
+  # Define metadata, including the startup script
+  metadata = {
+    startup-script = file("${path.module}/startup-script-one.sh")
+  }
+  tags = ["security-onion"]
+
+  encryption = {
+    #kms_key_self_link = module.kms.keys.key-so.id
+    kms_key_self_link = "projects/${var.project_id}/locations/${var.location}/keyRings/${var.keyring.name}/cryptoKeys/key-so"
+  }
+  service_account = {
+    email = google_service_account.compute.email
+  }
+  # Persistent Disk Attached to the Compute Engine with KMS
+  attached_disks = [
+    {
+      auto_delete = var.auto_delete
+      size        = 100
+      name        = "data-disk"
+      initialize_params = {
+        image = "ubuntu-2004-focal-v20240614"
       }
       #kms_key_self_link = module.kms.keys.key-so.id
       kms_key_self_link = "projects/${var.project_id}/locations/${var.location}/keyRings/${var.keyring.name}/cryptoKeys/key-so"
@@ -133,64 +189,11 @@ module "nat" {
   region         = var.location
   name           = "nat-secutity-onion"
   router_network = module.vpc.name
-
   #depends_on     = [module.vpc, module.kms]
   depends_on = [module.vpc]
 }
 
-# Google KMS Module
-# module "kms" {
-#   source     = "../../../modules/kms"
-#   project_id = var.project_id
-#   keys       = var.keys
-#   iam = {
-#     "roles/cloudkms.cryptoKeyEncrypterDecrypter" = [
-#       google_service_account.compute.member,
-#       "serviceAccount:service-${data.google_project.current.number}@compute-system.iam.gserviceaccount.com",
-#       "user:${var.email}"
-#     ]
-#   }
-#   keyring = var.keyring
-# }
 
-# Google VPC Module
-# module "vpc" {
-#   source                          = "../../../modules/net-vpc"
-#   project_id                      = var.project_id
-#   name                            = "vpc-so-kc"
-#   auto_create_subnetworks         = false
-#   delete_default_routes_on_create = true
-#   routing_mode                    = "GLOBAL"
-#   subnets = [
-#     {
-#       name          = "subnet-securityoniona"
-#       region        = var.location
-#       description   = "Subnet a security onion public subnet"
-#       ip_cidr_range = "10.200.12.0/25"
-#       enable_private_access = false
-#     },
-#     # custom description and PGA disabled
-#     {
-#       name                  = "subnet-securityoniona-no-pga-b"
-#       region                = var.location
-#       ip_cidr_range         = "10.200.12.128/25"
-#       description           = "Subnet b with no PGA"
-#       enable_private_access = false
-#   },
-#   # secondary ranges
-#     {
-#       name          = "subnet-so-secondary-ranges"
-#       region        = var.location
-#       ip_cidr_range = "10.200.13.0/25"
-#       description   = "Subnet c with secondary ranges"
-#       secondary_ip_ranges = {
-#         a = "192.168.0.0/24"
-#         b = "192.168.1.0/24"
-#       }
-#     }
-#   ]
-
-# }
 
 module "vpc" {
   source                  = "../../../modules/net-vpc"
@@ -211,8 +214,6 @@ module "vpc" {
   #depends_on = [module.kms]
 }
 
-# 10.200.11.0/24	
-
 # Google Computer Firewall
 resource "google_compute_firewall" "default" {
   name    = "allow-ssh-in"
@@ -223,20 +224,4 @@ resource "google_compute_firewall" "default" {
   # Allowing to connect only within the VPC CIDR Range
   source_ranges = ["0.0.0.0/0"]
   target_tags   = []
-  #source_ranges = var.source_ranges_allowed
 }
-
-
-# resource "google_compute_firewall" "allow-all-outbound" {
-#   name    = "allow-all-outbound"
-#   network = module.vpc.network.self_link  # Replace with your VPC name if different
-
-#   allow {
-#     protocol = "all"
-#     ports    = []
-#   }
-
-#   source_ranges = ["0.0.0.0/0"]  # Allow traffic from all sources (any IP)
-#   target_tags   = []             # No target tags (applies to all instances in the network)
-# }
- 
--- blueprints/il5/gcp-ce-security-onion/startup-script.sh
+++ blueprints/il5/gcp-ce-security-onion/startup-script.sh
@@ -1,48 +1,12 @@
 #!/bin/bash
 # Update package list and install necessary packages
-echo "apt-get update step 1" > /var/log/startup-script.log
 sudo apt-get update
-echo "apt-get install -y software-properties-common step 2" > /var/log/startup-script.log
 sudo apt-get install -y software-properties-common
 
 # Install Security Onion
-echo "add-apt-repository -y ppa:securityonion/stable step 3" > /var/log/startup-script.log
 sudo add-apt-repository -y ppa:securityonion/stable
-echo "sudo apt-get update step 4" > /var/log/startup-script.log
 sudo apt-get update
-echo "apt-get install -y securityonion-all step 5" > /var/log/startup-script.log
 sudo apt-get install -y securityonion-all
 
 # Start Security Onion setup (note: this is interactive; adjust as needed)
 sudo sosetup
-
-
-
-# Update and install necessary packages
-# echo "Hello, world step 1" > /var/log/startup-script.log
-# apt-get update
-# apt-get install -y curl wget
-
-# # Download and install Security Onion
-# wget https://github.com/Security-Onion-Solutions/securityonion/blob/master/securityonion-2.3.90.iso?raw=true -O /tmp/securityonion.iso
-# mkdir -p /mnt/so-iso
-# mount -o loop /tmp/securityonion.iso /mnt/so-iso
-
-# # Install necessary dependencies
-# apt-get install -y squashfs-tools
-# unsquashfs -d /tmp/so-extracted /mnt/so-iso/casper/filesystem.squashfs
-
-# # Run the Security Onion installer
-# /tmp/so-extracted/usr/sbin/securityonion-setup -y
-
-# # Add your custom Security Onion setup commands here
-# # ...
-
-# # Clean up
-# umount /mnt/so-iso
-# rm -rf /tmp/securityonion.iso /tmp/so-extracted
-
-# echo "Security Onion installation complete!" > /var/log/startup-script.log
- 
-# # Add your custom startup commands here
-
