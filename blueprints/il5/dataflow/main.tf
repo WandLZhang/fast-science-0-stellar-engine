6681c863b41e2613cec31e9878e7b99ddfb6b9ef
Dan Gordon <dan.gordon@darkwolfsolutions.com>
2024-07-24 01:32:40 -0400

    all working

--- blueprints/il5/dataflow/main.tf
+++ blueprints/il5/dataflow/main.tf
@@ -14,29 +14,34 @@
  * limitations under the License.
  */
 
-# Terraform Provider for Google Cloud Platform
 provider "google" {
   project = var.project_id
   region  = var.region
 }
 
 data "google_project" "current" {}
-data "google_compute_default_service_account" "default" {}
 
-resource "google_project_iam_member" "dataflow_worker" {
-  project = var.project_id
-  role    = "roles/dataflow.worker"
-  member  = "serviceAccount:${data.google_project.current.number}-compute@developer.gserviceaccount.com"
+module "kms" {
+  source     = "../../../modules/kms"
+  project_id = var.project_id
+  keys       = var.keys
+
+  iam = {
+    "roles/cloudkms.cryptoKeyEncrypterDecrypter" = concat(
+      [
+        "serviceAccount:service-${data.google_project.current.number}@compute-system.iam.gserviceaccount.com",                 # Compute Service Account Required by Dataflow
+        "serviceAccount:service-${data.google_project.current.number}@dataflow-service-producer-prod.iam.gserviceaccount.com", # Dataflow Service Account
+        "serviceAccount:${data.google_project.current.number}-compute@developer.gserviceaccount.com",                          # Worker Service account
+        "serviceAccount:service-${data.google_project.current.number}@gs-project-accounts.iam.gserviceaccount.com",            # GCS Service Account
+        # "serviceAccount:bq-${data.google_project.current.number}@bigquery-encryption.iam.gserviceaccount.com",
+        # "serviceAccount:service-${data.google_project.current.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+      ]
+    )
+  }
+
+  keyring = var.keyring
 }
 
-resource "google_project_iam_member" "dataflow_network_user" {
-  project = var.project_id
-  role    = "roles/compute.networkUser"
-  # role = "roles/compute.admin"
-  member = "serviceAccount:${data.google_project.current.number}-compute@developer.gserviceaccount.com"
-}
-
-# Google Cloud Storage Module
 module "gcs" {
   source         = "../../../modules/gcs"
   prefix         = var.prefix
@@ -48,86 +53,55 @@ module "gcs" {
 
   objects_to_upload = {
     sample-data = {
-      name         = "sample.txt"
-      source       = "input/sample.txt"
-      content_type = "text/csv"
+      name   = "input/sample.txt"
+      source = "sample.txt"
     }
+  }
+
+  iam = {
+    "roles/storage.objectAdmin" = concat(
+      [
+        "serviceAccount:service-${data.google_project.current.number}@compute-system.iam.gserviceaccount.com",                 # Compute Service Account
+        "serviceAccount:service-${data.google_project.current.number}@dataflow-service-producer-prod.iam.gserviceaccount.com", # Dataflow Service Account
+        "serviceAccount:${data.google_project.current.number}-compute@developer.gserviceaccount.com",                          # Worker Service account
+        # "serviceAccount:service-${data.google_project.current.number}@gs-project-accounts.iam.gserviceaccount.com",
+        # "serviceAccount:bq-${data.google_project.current.number}@bigquery-encryption.iam.gserviceaccount.com",
+        # "serviceAccount:service-${data.google_project.current.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+      ]
+    )
   }
 
   force_destroy = true
 
-  iam = {
-    "roles/storage.objectViewer" = concat(
-      [
-        "serviceAccount:${data.google_project.current.number}-compute@developer.gserviceaccount.com",
-      ]
-    ),
-    "roles/storage.objectCreator" = concat(
-      [
-        "serviceAccount:${data.google_project.current.number}-compute@developer.gserviceaccount.com",
-      ]
-    )
-  }
   depends_on = [module.kms]
 }
 
-# Google KMS Module
-module "kms" {
-  source     = "../../../modules/kms"
-  project_id = var.project_id
-  keys       = var.keys
-  iam = {
-    "roles/cloudkms.cryptoKeyEncrypterDecrypter" = concat(
-      [
-        "serviceAccount:service-${data.google_project.current.number}@dataflow-service-producer-prod.iam.gserviceaccount.com", # Dataflow Service Account
-        "serviceAccount:${data.google_project.current.number}-compute@developer.gserviceaccount.com",                          # Worker service account
-        "serviceAccount:service-${data.google_project.current.number}@gs-project-accounts.iam.gserviceaccount.com",
-        "serviceAccount:service-${data.google_project.current.number}@gcp-sa-pubsub.iam.gserviceaccount.com"]
-    )
-  }
-  keyring = var.keyring
+resource "google_project_iam_member" "worker_worker" {
+  project = var.project_id
+  role   = "roles/dataflow.worker"
+  member = "serviceAccount:${data.google_project.current.number}-compute@developer.gserviceaccount.com"
 }
 
-module "pubsub" {
-  source        = "../../../modules/pubsub"
-  project_id    = var.project_id
-  name          = "df-topic"
-  regions       = [var.region]
-  kms_key       = module.kms.keys.dataflow-job.id
-  depends_on    = [module.kms]
-  subscriptions = {}
-
-  iam = {
-    "roles/pubsub.subscriber" = concat(
-      [
-        "serviceAccount:${data.google_project.current.number}-compute@developer.gserviceaccount.com",
-        # "serviceAccount:service-${data.google_project.current.number}@gs-project-accounts.iam.gserviceaccount.com"
-      ]
-    ),
-    "roles/pubsub.viewer" = concat(
-      [
-        "serviceAccount:${data.google_project.current.number}-compute@developer.gserviceaccount.com",
-        # "serviceAccount:service-${data.google_project.current.number}@gs-project-accounts.iam.gserviceaccount.com"
-      ]
-    )
-  }
-}
-
-# Dataflow Job
 resource "google_dataflow_job" "job" {
-  name = var.dataflow_name
-  # template_gcs_path = var.template_gcs_path
-  template_gcs_path = "gs://dataflow-templates/latest/Word_Count"
+  project = var.project_id
+  name    = "dataflow-job"
+  region  = var.region
+  zone    = var.zone
+
+  template_gcs_path = var.template_gcs_path
   temp_gcs_location = "gs://${module.gcs.bucket.name}/temp"
-  # service_account_email = google_service_account.dataflow.email
-  project      = var.project_id
-  region       = var.region
-  network      = var.network_name
-  zone         = var.zone
-  kms_key_name = module.kms.keys.dataflow-job.id
+
   parameters = {
-    inputTopic      = "projects/${var.project_id}/topics/${module.pubsub.topic.name}"
-    outputTableSpec = "${var.project_id}:${google_bigquery_dataset.dataset.dataset_id}.${google_bigquery_table.table.table_id}"
+    inputFile = var.inputFile
+    output    = var.output
   }
-  depends_on = [module.gcs, module.kms]
+
+  network          = var.network_name
+  subnetwork       = var.subnet
+  ip_configuration = "WORKER_IP_PRIVATE"
+
+  kms_key_name = module.kms.keys.dataflow-job.id
+
+  depends_on = [module.kms, module.gcs]
+
 }
