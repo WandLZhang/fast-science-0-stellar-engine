a5c023e36e934d27417ea4b05a9f91d26cd10b71
Dan Gordon <dan.gordon@darkwolfsolutions.com>
2024-07-30 19:28:45 -0400

    changed to default for public access, changed key name, several other minor changes

--- blueprints/il5/gcs-project/variables.tf
+++ blueprints/il5/gcs-project/variables.tf
@@ -17,7 +17,6 @@
 variable "project_id" {
   description = "Project ID"
   type        = string
-  # TODO: Update the Project ID , example project-abc-123
 }
 
 variable "keyring" {
@@ -26,50 +25,18 @@ variable "keyring" {
     location = string
     name     = string
   })
-  default = {
-    location = "us-east4"
-    name     = "gcs-keyring"
-
-    # Example location = "us-east4"
-    # Example name     = "april"
-    # TODO: Update the name of the Key Ring, and location. The Location for IL5 can be us-east4 or us-central1
-  }
 }
-
-# variable "uniform_bucket_level_access" {
-#   description = "Enable or disable uniform bucket level access"
-#   type        = bool
-#   default     = true
-#   validation {
-#     condition     = contains([true, false], var.uniform_bucket_level_access)
-#     error_message = "uniform_bucket_level_access must be either true or false."
-#   }
-# }
-
-# variable "public_access_prevention" {
-#   description = "Enable or disable public access prevention"
-#   type        = bool
-#   default     = true
-#   validation {
-#     condition     = contains([true, false], var.public_access_prevention)
-#     error_message = "public_access_prevention must be either true or false."
-#   }
-# }
 
 variable "uniform_bucket_level_access" {
   description = "Enable or disable uniform bucket level access"
   type        = bool
   default     = true
-  validation {
-    condition     = var.uniform_bucket_level_access == true || var.uniform_bucket_level_access == false
-    error_message = "uniform_bucket_level_access must be either true or false."
-  }
 }
 
 variable "public_access_prevention" {
   description = "Enable or disable public access prevention"
   type        = string
-  default     = "inherited"
+  default     = "enforced"
   validation {
     condition     = contains(["enforced", "inherited"], var.public_access_prevention)
     error_message = "public_access_prevention must be either 'enforced' or 'inherited'."
@@ -80,7 +47,6 @@ variable "prefix" {
   description = "Optional prefix used to generate the bucket name."
   type        = string
   default     = "string"
-  # TODO: Update the name of the prefix, for example "dino"
   validation {
     condition     = var.prefix != ""
     error_message = "Prefix cannot be empty, please use null instead."
@@ -90,21 +56,17 @@ variable "prefix" {
 variable "name" {
   description = "Bucket name suffix."
   type        = string
-  # TODO: Update the name of the bucket suffix For example welcome-data
 }
 
 variable "location" {
   description = "Bucket location."
   type        = string
   default     = "us-east4"
-  # TODO: Update the Bucket Location. The Location for IL5 can be us-east4 or us-cental1
 }
 
 variable "email" {
-  # Example default = "admin.user-anme@example.google.com"
   description = "Email address of the user."
   type        = string
-  # TODO: Update the email address
 }
 
 variable "autoclass" {
