7c10e53f6f79242ae09fb9d12a31da9ff6d68342
Dan Gordon <dan.gordon@darkwolfsolutions.com>
2024-09-24 20:21:01 -0400

    fixed AW bug

--- fast/stages-aw/0-bootstrap/variables.tf
+++ fast/stages-aw/0-bootstrap/variables.tf
@@ -266,6 +266,11 @@ variable "prefix" {
   }
 }
 
+variable "bootstrap_project" {
+  description = "Bootstrap project."
+  type = string
+}
+
 variable "project_parent_ids" {
   description = "Optional parents for projects created here in folders/nnnnnnn format. Null values will use the organization as parent."
   type = object({
