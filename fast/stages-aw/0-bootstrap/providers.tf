c0a538620c951ae4f4a296508e20bb71c9e4f19a
Dan Gordon <dan.gordon@darkwolfsolutions.com>
2024-09-24 21:08:41 -0400

    moved providers.tf to providers.tf.tmp

--- fast/stages-aw/0-bootstrap/providers.tf
+++
@@ -1,6 +0,0 @@
-# Required for Assurd Workloads
-provider "google" {
-  project               = var.bootstrap_project
-  billing_project       = var.bootstrap_project
-  user_project_override = true
-}
