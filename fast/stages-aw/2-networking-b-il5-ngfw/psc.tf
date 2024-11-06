833983d3c0828abd51ede112d796fd4fcb5a6a06
Dan Gordon <dan.gordon@darkwolfsolutions.com>
2024-11-06 16:47:59 -0500

    added script for permissions and 1-resman default kms key

---
+++ fast/stages-aw/2-networking-b-il5-ngfw/psc.tf
@@ -0,0 +1,10 @@
+module "private_service_connect" {
+  source                     = "../../../modules/private-service-connect"
+  for_each = var.envs_folders
+
+  project_id = module.env-spoke-projects[each.key].project_id
+
+  network_self_link          = module.env-spoke-vpc[each.key].self_link
+  private_service_connect_ip = local.cidr_ranges["psc"]
+  forwarding_rule_target     = "all-apis"
+}
