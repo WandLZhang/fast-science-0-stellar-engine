fe14800ec96d0193c7638486e84fb90b186cc0f5
Dan Gordon <dan.gordon@darkwolfsolutions.com>
2024-11-07 09:51:11 -0500

    removed psc ip

--- fast/stages-aw/2-networking-a-fedramp-high/psc.tf
+++ fast/stages-aw/2-networking-a-fedramp-high/psc.tf
@@ -5,6 +5,6 @@ module "private_service_connect" {
   project_id = module.env-spoke-projects[each.key].project_id
 
   network_self_link          = module.env-spoke-vpc[each.key].self_link
-  private_service_connect_ip = local.cidr_ranges["psc"]
+  private_service_connect_ip = "10.3.0.5"
   forwarding_rule_target     = "all-apis"
 }
