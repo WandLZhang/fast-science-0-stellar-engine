a0fa8dd9f46ad21da9155e7989f38b969b595ee7
Dan Gordon <dan.gordon@darkwolfsolutions.com>
2024-11-07 12:53:19 -0500

    duplicated frh changes in il5

--- fast/stages-aw/2-networking-b-il5-ngfw/psc.tf
+++ fast/stages-aw/2-networking-b-il5-ngfw/psc.tf
@@ -1,10 +1,18 @@
-module "private_service_connect" {
-  source   = "../../../modules/private-service-connect"
+resource "google_compute_global_address" "private_service_connect_ip" {
   for_each = var.envs_folders
 
-  project_id = module.env-spoke-projects[each.key].project_id
+  project = module.env-spoke-vpc[each.key].project_id
+  name          = "default-peering"
+  purpose       = "VPC_PEERING"
+  # purpose = "PRIVATE_SERVICE_CONNECT"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = module.env-spoke-vpc[each.key].self_link
+}
 
-  network_self_link          = module.env-spoke-vpc[each.key].self_link
-  private_service_connect_ip = "10.3.0.5"
-  forwarding_rule_target     = "all-apis"
+resource "google_service_networking_connection" "default" {
+  for_each = var.envs_folders
+  network                 = module.env-spoke-vpc[each.key].self_link
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_service_connect_ip[each.key].name]
 }
