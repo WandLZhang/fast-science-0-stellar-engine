9659f462faf64fd9009f9222e225bc5fc7228621
Dan Gordon <dan.gordon@darkwolfsolutions.com>
2024-11-07 13:07:06 -0500

    tf fmt

--- fast/stages-aw/2-networking-b-il5-ngfw/psc.tf
+++ fast/stages-aw/2-networking-b-il5-ngfw/psc.tf
@@ -2,8 +2,8 @@ resource "google_compute_global_address" "private_service_connect_ip" {
   for_each = var.envs_folders
 
   project = module.env-spoke-vpc[each.key].project_id
-  name          = "default-peering"
-  purpose       = "VPC_PEERING"
+  name    = "default-peering"
+  purpose = "VPC_PEERING"
   # purpose = "PRIVATE_SERVICE_CONNECT"
   address_type  = "INTERNAL"
   prefix_length = 16
@@ -11,7 +11,7 @@ resource "google_compute_global_address" "private_service_connect_ip" {
 }
 
 resource "google_service_networking_connection" "default" {
-  for_each = var.envs_folders
+  for_each                = var.envs_folders
   network                 = module.env-spoke-vpc[each.key].self_link
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_service_connect_ip[each.key].name]
