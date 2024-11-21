ce5320e14031b593114c504068c24bc51f7990c9
Nicholas Hobin <nicholas.hobin@darkwolfsolutions.com>
2024-11-21 15:02:08 -0500

    tf fmt

--- blueprints/fedramp-high/workflows/main.tf
+++ blueprints/fedramp-high/workflows/main.tf
@@ -1,11 +1,11 @@
 module "workflows" {
-  source = "../../../modules/workflows"
-  project = var.project
-  name                = var.name
-  region              = var.region
-  description         = var.description
-  logging_level      = var.logging_level
-  env_vars       = var.env_vars
-  key     = var.key
-  file = var.file
+  source        = "../../../modules/workflows"
+  project       = var.project
+  name          = var.name
+  region        = var.region
+  description   = var.description
+  logging_level = var.logging_level
+  env_vars      = var.env_vars
+  key           = var.key
+  file          = var.file
 }
