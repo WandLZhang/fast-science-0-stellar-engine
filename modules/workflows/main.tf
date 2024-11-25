19259fc0c84daa15d53548a1262e690391221a50
Nicholas Hobin <nicholas.hobin@darkwolfsolutions.com>
2024-11-25 12:15:04 -0500

    commented out deletion_protection

--- modules/workflows/main.tf
+++ modules/workflows/main.tf
@@ -7,7 +7,7 @@ resource "google_workflows_workflow" "workflow" {
   description         = var.description
   service_account     = var.service_account
   call_log_level      = var.logging_level
-  deletion_protection = false
+  # deletion_protection = false               # Not recommended for production environment
   user_env_vars       = var.env_vars
   crypto_key_name     = var.key
 
