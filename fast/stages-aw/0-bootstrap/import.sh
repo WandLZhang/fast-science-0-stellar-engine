e41939fef61a52bd5d4f499b682d7eefdb6c2f45
Preston Church <preston.church@darkwolfsolutions.com>
2025-05-13 11:35:36 -0600

    dr-771-import-script-refactor

--- fast/stages-aw/0-bootstrap/import.sh
+++ fast/stages-aw/0-bootstrap/import.sh
@@ -1,10 +1,53 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-ORG=$(gcloud organizations list | grep -v 'DISPLAY_NAME' | awk '{print $2}')
-if [ -z "$ORG" ]; then
-  echo "Error: Failed to get organization ID."
+# Assign Organization ID
+ORG=$(gcloud organizations list --format='value(ID)')
+if [[ -z "${ORG}" ]]; then
+  echo "Error: Failed to get organization ID." >&2
   exit 1
 fi
-export ORG
 
-gcloud resource-manager org-policies list --organization="$ORG" | grep 'SET' | awk -F '[/ ]' '{print $2}'  | xargs -n 1 -I {} terraform import "module.organization.google_org_policy_policy.default[\"{}\"]" "organizations/$ORG/policies/{}"
+# Import Organization Policies Filtered by "SET" Constraints
+policies=$(gcloud resource-manager org-policies list \
+  --organization="${ORG}" \
+  --filter='booleanPolicy.enforced=true OR listPolicy:*' \
+  --format='value(constraint)')
+
+if [[ -z "${policies}" ]]; then
+  echo "No organization policies found matching the filter."
+else
+  echo -e "Importing policies...\n"
+
+  failed_imports=()
+
+  # Policy Iteration
+  while IFS= read -r constraint_path; do
+    constraint_name=${constraint_path##*/}
+    constraint_self_link="organizations/${ORG}/policies/${constraint_name}"
+
+    # Check Policy State
+    if terraform state show "module.organization.google_org_policy_policy.default[\"${constraint_name}\"]" > /dev/null 2>&1; then
+      # Skip Import
+      echo "${constraint_name} already managed, skipping import."
+    else
+
+      # Attempt Import
+      if ! terraform import \
+        "module.organization.google_org_policy_policy.default[\"${constraint_name}\"]" \
+        "${constraint_self_link}"; then
+        echo "Error: terraform import failed for constraint: ${constraint_name}" >&2
+        failed_imports+=("${constraint_name}")
+      fi
+    fi
+  done <<< "$policies"
+
+  if [[ ${#failed_imports[@]} -gt 0 ]]; then
+    echo -e "\nError: The following organizational policies failed to import:" >&2
+    for failed_policy in "${failed_imports[@]}"; do
+      echo "- ${failed_policy}" >&2
+    done
+    exit 1
+  else
+    echo -e "\nOrganization Policy import operation is complete!"
+  fi
+fi
