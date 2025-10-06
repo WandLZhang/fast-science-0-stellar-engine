7d8c174799bbdb787ff7c5e3b928dcf266656620
Michael McConachie <mikemcconachie@google.com>
2025-10-06 12:10:03 -0400

    moved into subdir

--- blueprints/fedramp-high/agent-space/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "admin_group_email" {
-  value       = google_workspace_group.admins.email
-  description = "The email address of the AgentSpace administrators group."
-}
-
-output "user_group_email" {
-  value       = google_workspace_group.users.email
-  description = "The email address of the AgentSpace users group."
-}
